652ddd8ec5f24563551611b47ff1f1350116c531
Chinmay Mathur <167203443+chinmaymathur@users.noreply.github.com>
2024-07-29 13:12:29 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,65 @@
+print("hello")
+a = 4
+b = 5
+print(a)
+print(b)
+name = "Chinmay"
+print(name)
+name = "mohit"
+print (name[0])
+student = "Raman"
+print(student[1])
+#next line mei  convert krne ke liye 
+college  = """ hello my 
+name is ritik"""
+print(college)
+
+cls = "second_year"
+new_variable = name +" " + cls
+
+print(new_variable)
+name = "Hello my name is chinmay" 
+
+#length batata hai 
+print(len(name))
+
+print(name[0:12])
+#replace
+student= name.replace('chinmay','aayush')
+print(student)
+#capital letter
+SKIT = name.upper()
+print(SKIT)
+#found the things
+intro = "Hello world"
+found="world" in intro
+print(found)
+
+
+#add
+a = (1,2,5,4)
+b= ( 4,66,8,6)
+print(a+b)
+a = (1,5,3)
+print(a.index(2))
+
+#list 
+callable = [1,2,3,4,5,6]
+print(callable(3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
